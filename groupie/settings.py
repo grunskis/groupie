e74c6cc13c67c9a7ccae0710548a983b063675ff
Martins Grunskis <martins@grunskis.com>
2013-09-28 10:19:09 +0200

    fix db setting

--- groupie/settings.py
+++ groupie/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
